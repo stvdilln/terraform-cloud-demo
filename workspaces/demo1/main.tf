c2d349e5dea408d8ce57a0042381760a3488c75b
Steve Dillon <steve.dillon@yaponcha.com>
2020-02-06 18:36:36 -0500

    mark the input sensitive

--- workspaces/demo1/main.tf
+++ workspaces/demo1/main.tf
@@ -2,6 +2,7 @@ output secrets {
     # this is not a sensitive item, so just output it
     value = "${var.testvalues.secret1} ${var.testvalues.secret2}"
 }
+
 resource null_resource nothing {
     provisioner "local-exec" {
     command = "echo ${var.testvalues.secret1}"
