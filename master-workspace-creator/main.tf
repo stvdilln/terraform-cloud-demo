a53b635ba538a144453ecb813e31152517387e5c
Steve Dillon <steve.dillon@yaponcha.com>
2020-02-06 17:39:59 -0500

    Try passing secrets as a map

--- master-workspace-creator/main.tf
+++ master-workspace-creator/main.tf
@@ -18,27 +18,36 @@ resource tfe_workspace workspace {
 # Add the secrets that the job creator needs to pass to child jobs
 
 
-resource tfe_variable ssh_pub_key {
-   key = "root_ssh_public_key"
-   value = var.root_ssh_public_key
+# resource tfe_variable ssh_pub_key {
+#    key = "root_ssh_public_key"
+#    value = var.root_ssh_public_key
+#    category = "terraform"
+#    # A public key should not be secret, the private key is the crown jewel.
+#    sensitive = false
+#    workspace_id = tfe_workspace.workspace.id
+# }
+# resource tfe_variable aks_client_secret {
+#    key = "payg_subscription_client_secret"
+#    value = var.payg_subscription_client_secret
+#    category = "terraform"
+#    # Try to NEver Reveal this in statefiles our output
+#    sensitive = true
+#    workspace_id = tfe_workspace.workspace.id
+# }
+# resource tfe_variable root_password_all_machines {
+#    key = "root_password_all_machines"
+#    value = var.root_password_all_machines
+#    category = "terraform"
+#    # Try to NEver Reveal this in statefiles our output
+#    sensitive = true
+#    workspace_id = tfe_workspace.workspace.id
+# }
+resource tfe_variable testvalues {
+   key = "testvalues"
+   hcl = false
+   value = file("test.hcl")
    category = "terraform"
-   # A public key should not be secret, the private key is the crown jewel.
+   # Try to NEver Reveal this in statefiles our output
    sensitive = false
-   workspace_id = tfe_workspace.workspace.id
-}
-resource tfe_variable aks_client_secret {
-   key = "payg_subscription_client_secret"
-   value = var.payg_subscription_client_secret
-   category = "terraform"
-   # Try to NEver Reveal this in statefiles our output
-   sensitive = true
-   workspace_id = tfe_workspace.workspace.id
-}
-resource tfe_variable root_password_all_machines {
-   key = "root_password_all_machines"
-   value = var.root_password_all_machines
-   category = "terraform"
-   # Try to NEver Reveal this in statefiles our output
-   sensitive = true
    workspace_id = tfe_workspace.workspace.id
 }
