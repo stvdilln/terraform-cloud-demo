0ad82c749637a35f8d51ec53fd4002489520c6af
Steve Dillon <steve.dillon@yaponcha.com>
2020-02-06 18:32:02 -0500

    remove null resource

--- workspaces/demo1/main.tf
+++ workspaces/demo1/main.tf
@@ -2,7 +2,6 @@ output secrets {
     # this is not a sensitive item, so just output it
     value = "${var.testvalues.secret1} ${var.testvalues.secret2}"
 }
-provider null_resource {}
 resource null_resource nothing {
     provisioner "local-exec" {
     command = "echo ${var.testvalues.secret1}"
