96a77159d2857ab7c52a395285d6aeaead711a64
Steve Dillon <steve.dillon@yaponcha.com>
2020-02-06 18:28:24 -0500

    make the job do something

--- workspaces/demo1/main.tf
+++ workspaces/demo1/main.tf
@@ -2,3 +2,8 @@ output secrets {
     # this is not a sensitive item, so just output it
     value = "${var.testvalues.secret1} ${var.testvalues.secret2}"
 }
+null_resource nothing {
+    provisioner "local-exec" {
+    command = "echo ${var.testvalues.secret1}"
+  }
+}
